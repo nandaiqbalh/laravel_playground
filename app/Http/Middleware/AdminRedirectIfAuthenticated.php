b2acfc1e64b58e09ff06a5aac8ceb4674c0bd418
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-01-08 18:16:51 +0700

    [Admin Auth] Create Admin Middleware

--- app/Http/Middleware/AdminRedirectIfAuthenticated.php
+++ app/Http/Middleware/AdminRedirectIfAuthenticated.php
@@ -7,15 +7,15 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class RedirectIfAuthenticated
+class AdminRedirectIfAuthenticated
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param  \Closure  $next
      * @param  string|null  ...$guards
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @return mixed
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
